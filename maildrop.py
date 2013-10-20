15d439a904a87c1292f2cc53b93a9bd5ca2a8033
Jeff Bryner <jeff@jeffbryner.com>
2013-10-20 15:43:03 -0700

    use check_output instead to avoid potential command injection update string handling to unicode for python3

--- maildrop.py
+++ maildrop.py
@@ -2,24 +2,30 @@
 import os
 import sys
 import maildropper
-from subprocess import getoutput
+from subprocess import check_output
+from subprocess import CalledProcessError
 
 deliverytarget='{0}@{1}'.format(os.environ['EXT'],os.environ['HOST'])
 print('qmail env asks for delivery to {0}\n'.format(deliverytarget))
 
 #alias?
 #check for valias output: alas@somedomain.com -> realacct@otherdomain.com
-alias=getoutput('/var/vpopmail/bin/valias {0}'.format(deliverytarget))
-if len(alias):
-    deliverytarget=alias.split('->')[1].strip()
+try:
+    alias=check_output(['/var/vpopmail/bin/valias',deliverytarget],shell=False)
+    if len(alias):
+        deliverytarget=alias.decode('ascii','ignore').strip().split('->')[1].strip()
+except CalledProcessError:
+    #no alias
+    pass
 
-#get homedir/check valid user    
-homedir=getoutput('/var/vpopmail/bin/vuserinfo -d {0}'.format(deliverytarget))
-if 'no such user' in homedir:
+#get homedir/check valid user
+try:
+    homedir=check_output(['/var/vpopmail/bin/vuserinfo','-d',deliverytarget],shell=False)
+except CalledProcessError:
     print('exiting..no such user')
     sys.exit(1)
 
-homedir=homedir+'/.maildir'
+homedir=homedir.decode('ascii','ignore').strip()+'/.maildir'
 print("Homedir is " +homedir)
 
 #init maildropper (sucks in stdin)
